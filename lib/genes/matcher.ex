2f4bdfce5fd32958e3b5fa596b14f9e1c00fcfd8
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-04-06 10:58:14 +0200

    Remove redundant conditions

--- lib/genes/matcher.ex
+++ lib/genes/matcher.ex
@@ -1,8 +1,6 @@
 defmodule Genes.Matcher do
   def matching_chars(seq1, seq2), do: _matching_chars(seq1, seq2)
 
-  defp _matching_chars(_a, ""), do: 0
-  defp _matching_chars("", _a), do: 0
   defp _matching_chars(a, a), do: String.length(a)
   defp _matching_chars(a, b) do
     a2 = String.slice(a, 1..-1)
