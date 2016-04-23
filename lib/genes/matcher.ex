f8a17c91f69419e99c0962a13413ca759252569a
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-04-23 11:27:12 +0200

    Add Matcher.sorted_successors

--- lib/genes/matcher.ex
+++ lib/genes/matcher.ex
@@ -7,4 +7,11 @@ defmodule Genes.Matcher do
     b2 = String.slice(b, 0..-2)
     _matching_chars(a2, b2)
   end
+
+  def sorted_successors(given_sequence, all) do
+    all
+    |> Enum.map(fn (seq) -> {seq, matching_chars(given_sequence, seq)} end)
+    |> Enum.sort_by(fn (tuple) -> elem(tuple, 1) end, &>=/2)
+    |> Enum.drop_while(fn (tuple) -> elem(tuple, 0) == given_sequence end)
+  end
 end
